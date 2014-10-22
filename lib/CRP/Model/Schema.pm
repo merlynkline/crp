2703357d8792d96b472202a9c2091b76447df92b
Merlyn Kline <merlyn@binary.co.uk>
2014-10-22 21:43:27 +0100

    Proper definition for enquiry table. Reset experimental deployment.

--- lib/CRP/Model/Schema.pm
+++ lib/CRP/Model/Schema.pm
@@ -3,7 +3,7 @@ package CRP::Model::Schema;
 use strict;
 use warnings;
 
-our $VERSION = 1;
+our $VERSION = 2;
 
 use base 'DBIx::Class::Schema';
 
--- lib/CRP/Model/Schema/Result/Enquiry.pm
+++ lib/CRP/Model/Schema/Result/Enquiry.pm
@@ -6,20 +6,62 @@ use warnings;
 
 use base 'DBIx::Class::Core';
 
-__PACKAGE__->load_components('InflateColumn::DateTime');
+__PACKAGE__->load_components(qw(InflateColumn::DateTime));
 __PACKAGE__->table('enquiry');
-
 __PACKAGE__->add_columns(
     'id' => {
-        data_type         => 'integer',
-        is_auto_increment => 1,
-        is_nullable       => 0,
-        sequence          => 'enquiry_id_seq',
+        data_type           => 'integer',
+        is_auto_increment   => 1,
+        is_nullable         => 0,
+        sequence            => 'enquiry_id_seq',
+        is_numeric          => 1,
+        auto_nextval        => 1,
     },
-    'email' => { data_type => 'varchar', is_nullable => 0, size => 255 },
+    name => {
+        data_type           => 'text',
+        is_nullable         => 1,
+    },
+    'email' => {
+        data_type           => 'text',
+        is_nullable         => 0,
+    },
+    create_date => {
+        data_type           => 'timestamp',
+        default_value       => \'SYSDATE',
+        is_nullable         => 0,
+    },
+    suspend_date => {
+        data_type           => 'timestamp',
+        is_nullable         => 1,
+    },
+    location => {
+        data_type           => 'text',
+        is_nullable         => 1,
+    },
+    latitude => {
+        data_type           => 'real',
+        is_nullable         => 1,
+    },
+    longitude => {
+        data_type           => 'real',
+        is_nullable         => 1,
+    },
+    notify_new_courses => {
+        data_type           => 'boolean',
+        is_nullable         => 1,
+    },
+    notify_tutors => {
+        data_type           => 'boolean',
+        is_nullable         => 1,
+    },
+    send_newsletter => {
+        data_type           => 'boolean',
+        is_nullable         => 1,
+    },
 );
 
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->add_unique_constraint('enquiry_email_key', ['email']);
 
 1;
+
