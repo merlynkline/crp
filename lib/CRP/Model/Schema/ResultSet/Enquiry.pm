929ea217d10659e87b32a6120ed8f1999cc073f4
Merlyn Kline <merlyn@binary.co.uk>
2014-12-07 12:18:24 +0000

    Tidy up and bugfix search for nearby enquiries

--- lib/CRP/Model/Schema/ResultSet/Enquiry.pm
+++ lib/CRP/Model/Schema/ResultSet/Enquiry.pm
@@ -13,17 +13,24 @@ sub search_near_location {
     my $self = shift;
     my($latitude, $longitude, $distance, $condition) = (shift, shift, shift, shift);
 
-    return if $latitude < -90 || $latitude > 90 || $longitude < 0 || $longitude >= 360;
+    return if $latitude < -90 || $latitude > 90 || $longitude <= -180 || $longitude >= 180;
     my $latitude_offset = $distance / KM_PER_DEGREE_LATITUDE;
-    my $longitude_offset = cos($latitude * PI / 180) * KM_PER_DEGREE_LATITUDE;
+    my $longitude_offset = $distance / (cos($latitude * PI / 180) * KM_PER_DEGREE_LATITUDE);
     my $location_condition = [
         -and => [
-            -and => [ latitude => {'>', $latitude - $latitude_offset}, {'<', $latitude + $latitude_offset} ],
-            -and => [ longitude => {'>', $longitude - $longitude_offset}, {'<', $longitude + $longitude_offset} ],
+            latitude => {'>', $latitude - $latitude_offset},
+            latitude => {'<', $latitude + $latitude_offset}
         ]
     ];
-    $condition = $condition ? [ -and => $location_condition, [ $condition ] ] : $location_condition;
-    return $self->search($condition, @_);
+    my $nearby_resultset = $self->search($location_condition);
+    $location_condition = [
+        -and => [
+            longitude => {'>', $longitude - $longitude_offset},
+            longitude => {'<', $longitude + $longitude_offset}
+        ]
+    ];
+    $nearby_resultset = $nearby_resultset->search($location_condition);
+    return $nearby_resultset->search($condition);
 }
 
 1;
