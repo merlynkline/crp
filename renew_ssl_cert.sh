0888161361d43d1f311399415c3214af230742f5
Merlyn Kline <merlyn@binary.co.uk>
2016-07-02 06:26:21 +0100

    Add path to carton command

--- renew_ssl_cert.sh
+++ renew_ssl_cert.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-carton exec le.pl --key ssl/lets_encrypt_account.key --csr ssl/kidsreflex.co.uk.csr --csr-key ssl/kidsreflex.co.uk.key --crt ssl/kidsreflex.co.uk.crt --domains "www.kidsreflex.co.uk" --generate-missing --handle-with Crypt::LE::Challenge::File --handle-params '{"public_doc_path": "public"}' --live
+/usr/local/bin/carton exec le.pl --key ssl/lets_encrypt_account.key --csr ssl/kidsreflex.co.uk.csr --csr-key ssl/kidsreflex.co.uk.key --crt ssl/kidsreflex.co.uk.crt --domains "www.kidsreflex.co.uk" --generate-missing --handle-with Crypt::LE::Challenge::File --handle-params '{"public_doc_path": "public"}' --live
