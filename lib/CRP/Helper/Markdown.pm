e9e1b830aeefbc524668da3c04dcb5a464719a23
Merlyn Kline <merlyn@binary.co.uk>
2017-05-04 20:24:06 +0100

    Refactor a bit for brevity

--- lib/CRP/Helper/Markdown.pm
+++ lib/CRP/Helper/Markdown.pm
@@ -9,13 +9,13 @@ sub register {
     my $c = shift;
     my($app, $config) = @_;
 
-    my %config = %$config if $config;
+    my %config = %$config if ref $config eq 'HASH';
     $app->helper(markdown => sub {
         my $c = shift;
         my $content = pop;
 
         my %this_config = (%config, @_);
-        $content = $content->()->to_string if ref $content eq 'CODE';
+        $content = $content->() if ref $content eq 'CODE';
         $content = markdown("$content", \%this_config);
         return Mojo::ByteStream->new($content);
     });
