090e45d59ce9058901ed737c1d14ea08bd71896c
Merlyn Kline <merlyn@binary.co.uk>
2018-01-25 23:24:16 +0000

    Validate uploaded olc PDFs

--- lib/CRP/Controller/OLCAdmin/Component/PDF.pm
+++ lib/CRP/Controller/OLCAdmin/Component/PDF.pm
@@ -26,6 +26,8 @@ sub save {
     my $c = shift;
 
     my $pdf_file_name = $c->_process_uploaded_pdf // $c->crp->trimmed_param('pdf_file');
+    $c->validation->error(upload => ['file_required']) unless $pdf_file_name;
+    return $c->edit if $c->validation->has_error;
     die "Couldn't process PDF" unless $pdf_file_name;
 
     my $component = $c->_component;
@@ -42,34 +44,62 @@ sub save {
 sub _process_uploaded_pdf {
     my $c = shift;
 
+    my $validation_error;
+    my $actual_file_name;
+
     my $pdf = $c->req->upload('upload');
-    return unless $pdf->size;
 
     if($pdf->size > ($c->config->{olc}->{max_pdf_file_size} || 10_000_000)) {
-        $c->validation->error(upload => ['file_too_large']);
+        $validation_error = 'file_too_large';
+    }
+    elsif($pdf->size < 1) {
+        return;
+    }
+    else {
+        use File::Temp;
+        my($fh, $temp_file) = tmpnam();
+        close $fh;
+
+        my $error;
+        try {
+            $pdf->move_to($temp_file);
+            use File::Copy;
+            $actual_file_name = $c->_get_unique_uploaded_pdf_file_name($c->req->upload('upload')->filename);
+            move $temp_file, $actual_file_name or die "Failed to move '$temp_file' to '$actual_file_name': $!";
+        }
+        catch {
+            $error = $_;
+        };
+        unlink $temp_file;
+        die $error if $error;
+
+        $validation_error = $c->_validate_pdf($actual_file_name);
+        unlink $actual_file_name if $validation_error;
+    }
+
+    if($validation_error) {
+        $c->validation->error(upload => [$validation_error]);
         return;
     }
 
-    use File::Temp;
-    my($fh, $temp_file) = tmpnam();
-    close $fh;
-
-    my $actual_file_name;
-    my $error;
-    try {
-        $pdf->move_to($temp_file);
-        use File::Copy;
-        $actual_file_name = $c->_get_unique_uploaded_pdf_file_name($c->req->upload('upload')->filename);
-        move $temp_file, $actual_file_name or die "Failed to move '$temp_file' to '$actual_file_name': $!";
-    }
-    catch {
-        $error = $_;
-    };
-    unlink $temp_file;
-    die $error if $error;
-
     $actual_file_name =~ s/^.*\///;
     return $actual_file_name;
+}
+
+sub _validate_pdf {
+    my $c = shift;
+    my($pathname) = @_;
+
+    my $validation_error;
+    try {
+        my $pdf_doc = PDF::API2->open($pathname);
+        die "PDF version too high" unless $pdf_doc->version <= 1.4;
+    }
+    catch {
+        $validation_error = 'invalid_pdf';
+    };
+
+    return $validation_error;
 }
 
 sub _get_unique_uploaded_pdf_file_name {
